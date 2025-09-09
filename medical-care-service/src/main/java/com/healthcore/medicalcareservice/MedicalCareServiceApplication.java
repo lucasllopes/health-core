8966fb96a27779b55ca6a26b1cee452876dc1624
Lucas Lopes <lucas.llopes99@gmail.com>
2025-09-08 21:33:08 -0300

    first commit

---
+++ medical-care-service/src/main/java/com/healthcore/medicalcareservice/MedicalCareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthcore.medicalcareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCareServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalCareServiceApplication.class, args);
+    }
+
+}
