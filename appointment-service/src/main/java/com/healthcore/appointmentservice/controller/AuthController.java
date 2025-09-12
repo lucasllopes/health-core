665e3094d8242fdbb8db473ff99b190a1edae163
JoseVitorAgatte <jvoaht@gmail.com>
2025-09-12 19:20:50 -0300

    implementacao de seguranca jwt

---
+++ appointment-service/src/main/java/com/healthcore/appointmentservice/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.healthcore.appointmentservice.controller;
+
+import com.healthcore.appointmentservice.dto.AuthResponseDTO;
+import com.healthcore.appointmentservice.dto.LoginRequestDTO;
+import com.healthcore.appointmentservice.security.JwtUtil;
+import com.healthcore.appointmentservice.service.UsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    private final UsuarioService usuarioService;
+
+    private final JwtUtil jwtUtil;
+
+    public AuthController(UsuarioService usuarioService, JwtUtil jwtUtil) {
+        this.usuarioService = usuarioService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO dto) {
+        log.info("Handling POST request to /usuarios");
+
+        usuarioService.validateCredentials(dto.login(), dto.password());
+
+        String acessToken = jwtUtil.generateToken(dto.login());
+        String refreshToken = jwtUtil.generateRefreshToken(dto.login());
+
+        return ResponseEntity.ok(new AuthResponseDTO(acessToken, refreshToken));
+    }
+
+
+}
---
+++ appointment-service/src/main/java/com/healthcore/appointmentservice/security/JwtUtil.java
@@ -0,0 +1,104 @@
+package com.healthcore.appointmentservice.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Logger log = LoggerFactory.getLogger(JwtUtil.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.secret.refresh}")
+    private String refreshSecret;
+
+    private SecretKey jwtSecretKey;
+
+    private SecretKey refreshKey;
+
+    private final int jwtExpirationMs = 900_000;
+
+    private final int refreshExpirationMs = 900_000;
+
+    @PostConstruct
+    public void init() {
+        jwtSecretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        refreshKey = Keys.hmacShaKeyFor(refreshSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .issuer("HealthCore")
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(jwtSecretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .issuer("HealthCore")
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + refreshExpirationMs))
+                .claim("type", "refresh")
+                .signWith(refreshKey)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(jwtSecretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+
+    //TODO: talvez colocar a logica de validacao do refresh token e gerar um novo token quando cair na ExpiredJwtException
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(jwtSecretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (ExpiredJwtException ex) {
+            throw new RuntimeException("Token expirado. Realize novamente o login.");
+        } catch (UnsupportedJwtException | MalformedJwtException ex) {
+            log.error("Token invalido: {}", ex.getMessage());
+            return false;
+        } catch (Exception ex) {
+            log.error("Error message: {}", ex.getMessage());
+            return false;
+        }
+
+    }
+
+    public String refreshAccessToken(String refreshToken) {
+        Claims claims = Jwts.parser()
+                .verifyWith(refreshKey)
+                .build()
+                .parseSignedClaims(refreshToken)
+                .getPayload();
+
+        if (!"refresh".equals(claims.get("type"))) {
+            throw new IllegalArgumentException("Token não é um refresh válido.");
+        }
+
+        return generateToken(claims.getSubject());
+    }
+}
