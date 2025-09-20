d617b7caa1f7fd388be0125633b5cb1739bc1729
dainaradev <92407087+carlospaulinodevjava@users.noreply.github.com>
2025-09-20 14:39:26 -0300

    Incluindo validações nos DTO's
    Incluindo validações nos DTO's

--- appointment-service/src/main/java/com/healthcore/appointmentservice/dto/AppointmentRequestDTO.java
+++ appointment-service/src/main/java/com/healthcore/appointmentservice/dto/AppointmentRequestDTO.java
@@ -1,12 +1,15 @@
 package com.healthcore.appointmentservice.dto;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public record AppointmentRequestDTO(
-    Long patientId,
-    Long doctorId,
+    @NotNull Long patientId,
+    @NotNull Long doctorId,
     Long nurseId,
-    LocalDateTime appointmentDate,
-    String status,
-    String notes
+    @NotNull @FutureOrPresent LocalDateTime appointmentDate,
+    @NotNull @Size(min = 2, max = 30) String status,
+    @Size(max = 1000) String notes
 ) {}
--- appointment-service/src/main/java/com/healthcore/appointmentservice/dto/AppointmentUpdateDTO.java
+++ appointment-service/src/main/java/com/healthcore/appointmentservice/dto/AppointmentUpdateDTO.java
@@ -1,10 +1,12 @@
 package com.healthcore.appointmentservice.dto;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public record AppointmentUpdateDTO(
     Long nurseId,
-    LocalDateTime appointmentDate,
-    String status,
-    String notes
+    @FutureOrPresent LocalDateTime appointmentDate,
+    @Size(min = 2, max = 30) String status,
+    @Size(max = 1000) String notes
 ) {}
