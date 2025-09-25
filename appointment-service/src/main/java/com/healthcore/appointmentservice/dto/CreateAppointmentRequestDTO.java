b6b975269f7c1d012537ec7cdfdc77ed99a3a5eb
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-09-24 23:08:31 -0300

    Appointement ajustes após testes via postman
    Appointement ajustes após testes via postman

--- appointment-service/src/main/java/com/healthcore/appointmentservice/dto/CreateAppointmentRequestDTO.java
+++ appointment-service/src/main/java/com/healthcore/appointmentservice/dto/CreateAppointmentRequestDTO.java
@@ -1,6 +1,7 @@
 package com.healthcore.appointmentservice.dto;
 
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -9,6 +10,6 @@ public record CreateAppointmentRequestDTO(
         @NotNull Long doctorId,
         Long nurseId,
         @NotNull @Future LocalDateTime appointmentDate,
+        @NotBlank String status,
         String notes
 ) {}
-
